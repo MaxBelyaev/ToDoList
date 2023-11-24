ca89d75f5c486386b31369ecd677c200982d3cab
Max Belyaev <makcimbelaev@gmail.com>
2023-11-24 16:49:50 +0300

    added model.swift and checkmarks

--- ToDoList/Model.swift
+++ ToDoList/Model.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+var ToDoItems: [[String: Any]] = [
+    ["Name": "Позвонить маме", "isCompleted": true],
+    ["Name": "Дописать прилажение", "isCompleted": false],
+    ["Name": "Побегать голым на морозе", "isCompleted": false]
+]
+
+func addItem(nameItem: String, isCompleted: Bool = false) {
+    ToDoItems.append(["Name": nameItem, "isCompleted": isCompleted])
+    saveData()
+}
+
+func removeItem(at index: Int) {
+    ToDoItems.remove(at: index)
+    saveData()
+}
+
+func changeState(at item: Int) -> Bool {
+    ToDoItems[item]["isCompleted"] = !(ToDoItems[item]["isCompleted"] as! Bool)
+    saveData()
+    return ToDoItems[item]["isCompleted"] as! Bool
+}
+
+func saveData() {
+    print("Данные сохранены")
+}
+
+func loadData() {
+    
+}
--- ToDoList/TableViewController.swift
+++ ToDoList/TableViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class TableViewController: UITableViewController {
 
+    @IBAction func pushAddAction(_ sender: Any) {
+        addItem(nameItem: "New Item")
+        tableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,44 +28,56 @@ class TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return ToDoItems.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let currentItem = ToDoItems[indexPath.row]
+        cell.textLabel?.text = currentItem["Name"] as? String
+        
+        if (currentItem["isCompleted"] as? Bool) == true {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            removeItem(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if changeState(at: indexPath.row) {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+
+    }
+    
+    
     /*
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
